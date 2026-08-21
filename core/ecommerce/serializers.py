689043c0617b215dbae59c93e315b10aab6c756a
saiman lamichhane <saimanlamichhane0@gmail.com>
2026-08-21 09:58:58 +0545

    Update serializers.py

--- core/ecommerce/serializers.py
+++ core/ecommerce/serializers.py
@@ -1,1 +1,50 @@
+from rest_framework import serializers  # type: ignore
+from .models import Products, Brand, Customer, Order
+
+class productSerializer(serializers.ModelSerializer):
+    class  Meta:
+        model = Products
+        fields = [
+            'name',
+            'description',
+            'price',
+            'image',
+            'brand',
+            'category',
+        ]
+
+
+class BrandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = [
+            'id',
+            'name',
+            'logo',
+            'description',
+        ]
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = [
+            'id',
+            'first_name',
+            'last_name',
+            'address',
+            'phone_number',
+        ]
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = [
+            'id',
+            'customer',
+            'product',
+            'count',
+            'total_price',
+        ]
 
